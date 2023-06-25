23d7a6dbd0f369b3357ac27e0b795093d9e68399
Shahriar Hasan <sh.shahriar5@gmail.com>
2023-06-25 17:59:55 +0600

    Functionallity done

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet,
+   Text, 
+   View,
+    Button, 
+    TextInput, 
+    ScrollView, 
+    FlatList, 
+    Image 
+  } from 'react-native';
+import GoalItem from './components/GoalItems';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
+  const [modalIsVisible,setModalIsVisible] = useState(false);
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function startAddGoalHandler(){
+    setModalIsVisible(true);
+  }
+
+  function endAddGoalHandler(){
+    setModalIsVisible(false);
+  }
+
+  function addGoalHandler(enteredGoalText) {
+    setCourseGoals(currentCourseGoals => [
+      ...currentCourseGoals, { text: enteredGoalText, id: Math.random().toString() }
+    ]);
+    endAddGoalHandler();
+  };
+
+  function deleteGoalHandler(id) {
+    console.log(id);
+    setCourseGoals((currentCourseGoals) => {
+      return currentCourseGoals.filter((goals) => goals.id !== id);
+    })
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <Button title='Add New Goal'
+       color="#5e0acc"
+       onPress={startAddGoalHandler}
+       />
+     <GoalInput visible={modalIsVisible}
+      onAddGoal={addGoalHandler}
+      onCancel={endAddGoalHandler}
+       />
+      <View style={styles.goalsContainer}>
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return <GoalItem
+              text={itemData.item.text}
+              id={itemData.item.id}
+              onDelete={deleteGoalHandler}
+
+            />;
+          }}
+          alwaysBounceVertical={false}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
+
+  goalsContainer: {
+    flex: 4,
+
+  },
+
 });
---
+++ components/GoalInput.js
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import { View, TextInput, Button, StyleSheet, Modal,Image } from 'react-native'
+
+function GoalInput(props) {
+  const [enteredGoalText, setEnteredText] = useState('');
+
+  function goalInputHandler(enteredText) {
+    setEnteredText(enteredText);
+  };
+
+  function addGoalHandler() {
+    props.onAddGoal(enteredGoalText);
+    setEnteredText('');
+  }
+
+  return (
+    <Modal visible={props.visible} animationType='slide'>
+      <View style={styles.inputContainer}>
+        <Image style={styles.image} source={require('../assets/images/pin.jpg')}/>
+        <TextInput style={styles.textInput} placeholder='Your course goal'
+          value={enteredGoalText}
+          onChangeText={goalInputHandler}
+        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="Add Goal" onPress={addGoalHandler} color="#5e0acc" />
+          </View>
+          <View style={styles.Button}>
+            <Button title="Cancel" onPress={props.onCancel} color= "#f31282"/>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+}
+
+export default GoalInput;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding:16,
+    backgroundColor: '#311b6b',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '100%',
+    padding: 8,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: 16
+  },
+  button: {
+    width: 100,
+    marginHorizontal: 8
+  },
+  image:{
+    width: 100,
+    height: 100,
+    margin:20
+  }
+});
